49c44a18ebfc4524d887b5b1ed100b31be441b59
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-06-30 23:47:18 -0400

    create and style navbar

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,18 @@
 import React from 'react' 
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+// import { Home, Students, Campuses } from "../pages"
+import Home from "../pages/Home"
+import Campuses from "../pages/Campuses"
+import Students from "../pages/Students"
+import { Navbar } from "../components"
 import './App.css';
 
 function App() {
   return (
     <Router>
+    <div className="Navbar">
+    <Navbar/>
+    </div>
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />}/>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import Typography from '@mui/material/Typography';
+import { NavLink } from "react-router-dom";
+import "../styles/navbar.css";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div id="navbar">
+        <div className="home-navlink">
+          <NavLink to="/">
+            <div className="logo">
+              <div>
+                <img width="82" height="82" src="https://img.icons8.com/external-justicon-flat-justicon/64/external-graduation-elearning-and-education-justicon-flat-justicon.png" alt="external-graduation-elearning-and-education-justicon-flat-justicon"/>
+                </div>
+                <div className="typography">
+                <Typography
+                  variant="h2"
+                  sx={{ fontFamily: `'Tangerine', cursive`, textShadow: '1px 1px 2px black' }}
+                >
+                  Scholar
+                </Typography>
+                <Typography
+                  variant="h4"
+                  sx={{ marginLeft: '20px', fontFamily: `'Manrope', sans-serif`, transform: 'translate(13px,-20px)', fontWeight: '700', textShadow: '1px 1px 2px black',  
+                '&:hover': { color: 'var(--mint)'},}}
+                >
+                  CENTRAL
+                </Typography>
+              </div>
+            </div>
+          </NavLink>
+        </div>
+        <div className="side-navlinks">
+          <NavLink to="/campuses" className="navlink">Campuses</NavLink>
+          <NavLink to="/students" className="navlink">Students</NavLink>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
