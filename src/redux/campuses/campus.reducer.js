6216528e3b97f0e391e088663581dbeb227571f2
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-07-01 18:46:21 -0400

    add the redux for delete campus

--- src/redux/campuses/campus.reducer.js
+++ src/redux/campuses/campus.reducer.js
@@ -1,4 +1,4 @@
-import { FETCH_ALL_CAMPUSES, ADD_CAMPUS, DELETE_CAMPUS } from "./campus.types";
+import { FETCH_ALL_CAMPUSES, ADD_CAMPUS, EDIT_CAMPUS, DELETE_CAMPUS } from "./campus.types";
 
 export const INITIAL_CAMPUSES_STATE = {
     campusList: [],
@@ -13,6 +13,15 @@ const campusReducer = (state = INITIAL_CAMPUSES_STATE, action) => {
         case ADD_CAMPUS:
             //post request with new campus information, to be inserted into the campus list state by inserting the new campus directly into the campus list
             return {...state, campusList: [...state.campusList, action.payload]} 
+        case EDIT_CAMPUS: 
+            //put request edited campus information, find the campus by id (in route) then push the updated list back into the campus list
+            const updatedCampusList = state.campusList.map((campus) =>
+                campus.id === action.payload.id ? action.payload : campus
+            );
+            return { ...state, campusList: updatedCampusList }; 
+        case DELETE_CAMPUS:
+            //filter out any campus from the campus list with the id associated with the campus 
+            return { ...state, campusList: state.campusList.filter(campus => campus.id !== action.payload) };
         default:
             return state;
     }
