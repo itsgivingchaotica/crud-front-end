b1d5a41e0db5019de0b97fb09e0e467654dc5b6a
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-07-03 15:51:27 -0400

    trash search reducer not needed

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,14 +2,12 @@
 import { combineReducers } from "redux"
 import campusReducer from "./campuses/campus.reducer"
 import studentReducer from "./students/student.reducer"
-import searchReducer from "./searchItems/search.reducer"
 
 //function takes in object of all reducers to combine,
 //taking a key value pair 
 const rootReducer = combineReducers({
     campuses: campusReducer,
     students: studentReducer,
-    searchItems: searchReducer
 })
 
 export default rootReducer
--- src/redux/searchItems/search.types.js
+++
@@ -1,1 +0,0 @@
-export const SEARCH_ALL = 'SEARCH_ALL'
