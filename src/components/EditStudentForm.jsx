434200881383d2e998a46d67a6c65500a83a7f38
Andrea <Andrea.Tranchina82@gmail.com>
2023-07-04 01:06:31 -0400

    removed unused/commented code

--- src/components/EditStudentForm.jsx
+++ src/components/EditStudentForm.jsx
@@ -95,44 +95,9 @@ const EditStudentForm = ({handleChangeFirstName, handleChangeLastName, handleCha
       <div className="input-container">
         <Button id="btn-form-edit-student" type="submit" variant="contained" endIcon={<CheckRoundedIcon/>}>Done</Button>    
       </div>
-            {/* <input
-            type="text"
-            name="firstName"
-            value={editedStudent.firstName}
-            placeholder="First Name"
-            onChange={handleChangeFirstName}
-            />
-            <input
-            type="text"
-            name="lastName"
-            value={editedStudent.lastName}
-            placeholder="Last Name"
-            onChange={handleChangeLastName}
-            />
-            <input
-            type="email"
-            name="email"
-            value={editedStudent.email}
-            placeholder="Email"
-            onChange={handleChangeEmail}
-            />
-            <input
-            type="number"
-            name="gpa"
-            value={editedStudent.gpa}
-            placeholder="gpa"
-            onChange={handleChangeGpa}
-            />
-            <select defaultValue="choose" className="dropdown" onChange={handleChangeCampus}>
-                <option value="choose" disabled>Choose Campus</option>
-                {allCampuses.map((campus) => {
-                    return <option key={campus.id} value={campus.id} id={campus.name}>{campus.name + " - " + campus.id}</option>
-                })}  
-            </select>
-            <button type="submit">Done</button> */}
-        </form>
-        </>
-        </ThemeProvider>
+    </form>
+    </>
+    </ThemeProvider>
   )
 }
 
