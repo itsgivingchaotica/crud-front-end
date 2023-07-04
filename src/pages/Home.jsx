648e0a59ab4a33d9f38edd5ecb380ef8406cd4d8
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-07-04 13:02:54 -0400

    adjust padding home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -46,7 +46,7 @@ const Home = () => {
         <div className="home" style={{display:'flex', justifyContent:'center'}}>
           <Grid container sx={{maxHeight:'50vh', marginLeft:'80px'}}>
             <Grid item xs={6} md={7} style={{ display: 'flex', justifyContent: 'flex-end'}}>
-              <div style={{ color: 'white', transform: 'translateY(200px)', paddingTop:'20px',backgroundColor:'rgb(66, 55, 52,0.9)'}}>
+              <div style={{ color: 'white', transform: 'translateY(200px)', padding:'20px',backgroundColor:'rgb(66, 55, 52,0.9)'}}>
                 <Typography variant='h3' sx={{fontFamily: `'Ysabeau Infant', sans-serif`, fontWeight:'700', textShadow: '-1px 1px 0 black, -1px 1px 0 black, -1px 1px 0 black, -1px 1px 0 black'}}>
                   Experience software designed to make your academic operations 
                   <div style={{display: 'flex', justifyContent:'center'}}><Typography variant='h1'sx={{fontFamily:`'Tangerine', cursive`, color:'var(--celadon-2)'}}>faster</Typography>
