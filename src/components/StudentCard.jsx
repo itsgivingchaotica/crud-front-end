6d4ec824e7c3b1f18357a56ff7eb78ec56ae5872
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-07-05 23:18:01 -0400

    style and layout of all student profiles page

--- src/components/StudentCard.jsx
+++ src/components/StudentCard.jsx
@@ -1,21 +1,30 @@
 import React, {useEffect, useState} from 'react'
-import "../styles/studentCard.css";
 import { NavLink, useNavigate } from 'react-router-dom';
 import { deleteStudentThunk } from '../redux/students/student.actions';
 import { useDispatch, useSelector } from 'react-redux';
+import Button from '@mui/material/Button'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import Divider from '@mui/material/Divider'
+import Stack from '@mui/material/Stack'
+import Tooltip from '@mui/material/Tooltip'
+import Typography from '@mui/material/Typography'
+import Zoom from '@mui/material/Zoom'
 import DeleteButtonSnackbar from './DeleteButtonSnackbar';
+import EmailIcon from '@mui/icons-material/Email';
 import axios from 'axios';
+import "../styles/studentCard.css";
 
 const StudentCard = (props) => {
   const [enrolledCampus, setEnrolledCampus] = useState("");
-
+  const { id, firstName, lastName, email, imageUrl, gpa, campusId } = props;
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const handleClickDelete = () => {
     // let result = window.confirm("Are you sure you want to delete the student?");
     // if (result === true){
-      dispatch(deleteStudentThunk(props.id));
+      dispatch(deleteStudentThunk(id));
     // }
 
   }
@@ -27,7 +36,7 @@ const StudentCard = (props) => {
   useEffect(()=> {
     const fetchStudentCampus = async()=>{
       try {
-        const res = await axios.get(`http://localhost:8080/api/campuses/${props.campusId}`)
+        const res = await axios.get(`http://localhost:8080/api/campuses/${campusId}`)
         const campusResponse = res.data;
         setEnrolledCampus(campusResponse);
       } catch(error){
@@ -39,18 +48,87 @@ const StudentCard = (props) => {
   
   return (
 
-    <div>
-        <h1>Student</h1>
-        <h3>{props.firstName + " " + props.lastName}</h3>
-        <h3>{props.email}</h3>
-        <img className="student-image" src={props.imageUrl}></img>
-        <h3>{props.gpa}</h3>
-        <h3>campusId: {props.campusId}</h3>
-        {enrolledCampus.name?<h3 onClick={visitSingleCampusPage}>Enrolled Campus: {enrolledCampus.name}</h3>
-          :<h3>Enrolled Campus: Not Enrolled</h3>}
-        <DeleteButtonSnackbar handleClickDelete={handleClickDelete}/>
-        <NavLink to={`/students/${props.id}`}>Student Profile</NavLink>
-    </div>
+    <Card sx={{paddingBottom: '30px'}}>
+      {/* NAME: REQUIRED, FIRST, LAST*/}
+      <CardContent sx={{borderBottom:'4px solid black'}}>
+        <Typography 
+          variant="h4" 
+          className="name" 
+          sx={{fontFamily:`'Ysabeau Infant', sans-serif`, fontWeight:'700'}}
+        > 
+          {firstName} {lastName}
+        </Typography>
+      </CardContent>
+          <Stack direction='row'>
+          {/* IMAGE URL: DEFAULT REQUIRED */}
+            {(<CardContent
+              sx={{ display: 'flex', alignItems: 'center', height:'100%',width:'100%'}}>
+                <img src={imageUrl} alt={`${firstName} ${lastName} profile`}  styles={{justifyContent:'center' }}/>
+              </CardContent>)}
+            </Stack>
+            <Stack direction='row' justifyContent='space-between' width='100%'>
+            {/* STUDENT DETAILS AND OPTIONS */}
+            <Stack direction='column'>
+            {/* EMAIL: REQUIRED */}
+            <Tooltip title="CONTACT" TransitionComponent={Zoom}  arrow>
+              <CardContent sx={{ display:'flex', alignItems: 'center', width:'100%',  '&:hover': {
+          cursor: 'pointer', color:'var(--mint-2)'
+        , textShadow: '1px 1px 1px var(--dark-green)'},}}>
+                <EmailIcon sx={{width:'30px', height:'30px',color:'black'}}/>
+                <Typography 
+                  variant="h5" 
+                  sx={{fontFamily: `'Manrope',sans-serif`, marginLeft:'10px'}}>
+                    {email}
+                </Typography>
+              </CardContent>
+              </Tooltip>
+                <Divider light/>
+              {/* GPA: REQUIRED */}
+              <Tooltip title="SHOW TRANSCRIPT" TransitionComponent={Zoom}  arrow>
+              <CardContent sx={{ display: 'flex', alignItems: 'center', marginRight:'10px', '&:hover': {
+          cursor: 'pointer', color:'var(--indigo)', textShadow: '1px 1px 1px var(--dark-green)'}}}>
+                <img width="35" height="35" src="https://img.icons8.com/sf-regular-filled/48/report-card.png" alt="report-card"/>
+                <Typography 
+                  variant='h5'
+                  sx={{marginLeft:'10px', fontFamily:`'Manrope',sans-serif`, '&:hover':{textShadow: '1px 1px 1px var(--dark-green)'}}}> 
+                  {gpa} GPA 
+                </Typography>
+              </CardContent>
+              </Tooltip>
+               <Divider light/>
+                {/* CAMPUS ID: OPTIONAL */}
+                <Tooltip title="VISIT SITE" TransitionComponent={Zoom}  arrow>
+              <CardContent sx={{ display: 'flex', alignItems: 'center', '&:hover': {
+          cursor: 'pointer', color:'var(--gold)', textShadow: '1px 1px 1px var(--dark-green)'}}} onClick={visitSingleCampusPage}>
+                <div styles={{marginRight:'10px'}}>
+                  <img width="35" height="35" src="https://img.icons8.com/ios-glyphs/35/university-campus.png" alt="university-campus"/>
+                </div>
+                {/* SHOW CAMPUS IF ENROLLED */}
+                {enrolledCampus.name? (
+                  <Typography variant='h5' 
+                  sx={{fontFamily: `'Manrope',sans-serif`, marginLeft:'10px', '&:hover':{textShadow: '1px 1px 1px var(--dark-green)'}}}>
+                    {enrolledCampus.name}
+                  </Typography>) : ( <Typography variant='h5' 
+                    sx={{marginLeft:'10px'}}>Not Enrolled</Typography>)
+                  }
+                </CardContent>
+              </Tooltip>
+            </Stack>
+            
+          </Stack>
+          <CardContent sx={{marginLeft:'30px', justifyContent: 'center'}}>
+                <NavLink to={`/students/${id}`} style={{textDecoration:'none'}} >
+                  <Tooltip title="GO TO PROFILE" placement='left' arrow TransitionComponent={Zoom}>
+                    <Button variant='contained' color="success" sx={{marginRight:'10px', marginLeft:'-40px',flexDirection:'column', paddingLeft:'22px', paddingRight:'22px'}}>
+                      <img width="55" height="55" src="https://img.icons8.com/color/70/student-center.png" alt="student-center"/>
+                      Profile
+                    </Button>
+                  </Tooltip>
+                </NavLink>
+                {/* DELETE STUDENT AND UNDO SNACKBAR*/}
+                <DeleteButtonSnackbar handleClickDelete={handleClickDelete}/>
+              </CardContent>
+    </Card>
   )
 }
 
--- src/components/StudentListItems.jsx
+++ src/components/StudentListItems.jsx
@@ -1,15 +1,31 @@
 import React from 'react'
 import { StudentCard } from '../components'
+import { useMediaQuery } from '@mui/material'
+import Grid from '@mui/material/Grid'
 
 const StudentListItems = (props) => {
+
+  const isMediumScreen = useMediaQuery('(max-width: 900px)');
+  const isSmallScreen = useMediaQuery('(max-width: 700px')
+
   return (
-    <div>
+     <Grid container spacing={2} sx={{padding:'5px'}}>
     {props.allStudents.map((student)=>{
-        return (<StudentCard key={student.id} id={student.id} firstName={student.firstName} lastName={student.lastName}
-                email={student.email} imageUrl={student.imageUrl} gpa={student.gpa} campusId={student.campusId}>
-                </StudentCard>)
+        return (
+          <Grid item xs={isSmallScreen ? 12 : isMediumScreen ? 6 : 4} key={student.id} sx={{marginBottom: '50px'}}>
+            <StudentCard 
+              key={student.id} 
+              id={student.id} 
+              firstName={student.firstName} 
+              lastName={student.lastName}
+              email={student.email} 
+              imageUrl={student.imageUrl} 
+              gpa={student.gpa} 
+              campusId={student.campusId}>
+            </StudentCard>
+          </Grid>)
     })}
-    </div>
+    </Grid>
   )
 }
 
